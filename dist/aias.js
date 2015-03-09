27ccc1bde4877966aea0f08491744ea6ef52d91a
Tyler Walters <tyler@tylerwalters.com>
2015-03-09 12:14:44 -0700

    Compiling product files

--- dist/aias.js
+++ dist/aias.js
@@ -1,1 +1,70 @@
-(function(){"use strict";function e(e){try{return JSON.parse(e)}catch(t){return e}}function t(t,r,o){return new n(function(n,u){var i=window.XMLHttpRequest||ActiveXObject;req=new i("MSXML2.XMLHTTP.3.0"),req.open(t,r,!0),req.setRequestHeader("Content-type","application/x-www-form-urlencoded"),req.onreadystatechange=function(){if(4===this.readyState)if(200===this.status){var t=e(req.responseText);n(t)}else u(new Error("Request responded with status "+req.status))},req.send(o)})}var n="undefined"!=typeof module&&module.exports?require("promise"):this.Promise,r={};r.get=function(e){return t("GET",e)},r.post=function(e,n){return t("POST",e,n)},r.put=function(e,n){return t("PUT",e,n)},r["delete"]=function(e){return t("DELETE",e)},"function"==typeof define&&define.amd?define("aias",function(){return r}):"undefined"!=typeof module&&module.exports?module.exports=r:this.aias=r}).call(window);
+(function () {
+	'use strict';
+
+	var Promise = (typeof module !== 'undefined' && module.exports) ? require('promise') : this.Promise;
+
+	var aias = {};
+
+	function prepareResponse (res) {
+		try {
+			return JSON.parse(res);
+		}
+		catch (e) {
+			return res;
+		}
+	}
+
+	function request (type, url, data) {
+		return new Promise(function (fulfill, reject) {
+			// Set ajax to correct XHR type. Source: https://gist.github.com/jed/993585
+			var Xhr = window.XMLHttpRequest||ActiveXObject;
+			req = new Xhr('MSXML2.XMLHTTP.3.0');
+
+			req.open(type, url, true);
+			req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			req.onreadystatechange = function (e) {
+				if (this.readyState === 4) {
+					if (this.status === 200) {
+						var response = prepareResponse(req.responseText);
+						fulfill(response);
+					}
+					else {
+						reject(new Error('Request responded with status ' + req.status));
+					}
+				}
+			};
+			req.send(data);
+		});
+	}
+
+	aias.get = function (url) {
+		return request('GET', url);
+	};
+
+	aias.post = function (url, data) {
+		return request('POST', url, data);
+	};
+
+	aias.put = function (url, data) {
+		return request('PUT', url, data);
+	};
+
+	aias.delete = function (url) {
+		return request('DELETE', url);
+	};
+
+	// AMD module
+	if (typeof define === 'function' && define.amd) {
+		define('aias', function () {
+			return aias;
+		});
+	}
+	// Require.js module
+	else if (typeof module !== 'undefined' && module.exports) {
+		module.exports = aias;
+	}
+	// Global library
+	else {
+		this.aias = aias;
+	}
+}).call(window);
