c3aa6d073d84d0a7db1f2039ede94e12f63cae6f
Tyler Walters <tyler@tylerwalters.com>
2015-03-10 09:14:59 -0700

    Building with latest changes

--- dist/aias.js
+++ dist/aias.js
@@ -1,11 +1,24 @@
-(function () {
+/** 
+	* A standalone AJAX library using JavaScript promises.
+	*
+	* @namespace aias
+	*/
+
+(function (context) {
 	'use strict';
 
-	var Promise = (typeof module !== 'undefined' && module.exports) ? require('promise') : this.Promise;
+	var Promise = (typeof module !== 'undefined' && module.exports) ? require('promise') : context.Promise,
+			aias = {};
 
-	var aias = {};
-
+/**
+	* Attempts to parse the response as JSON otherwise returns it untouched.
+	* 
+	* @param {string} res The response to be processed.
+	* 
+	* @memberof aias
+	*/
 	function prepareResponse (res) {
+		console.log(res);
 		try {
 			return JSON.parse(res);
 		}
@@ -14,19 +27,28 @@
 		}
 	}
 
+/**
+	* Attempts to parse the response as JSON otherwise returns it untouched.
+	* 
+	* @param {string} type The HTTP verb to be used.
+	* @param {string} url The url for the XHR request.
+	* @param {object} data Optional. The data to be passed with a POST or PUT request.
+	* 
+	* @memberof aias
+	*/
 	function request (type, url, data) {
 		return new Promise(function (fulfill, reject) {
 			// Set ajax to correct XHR type. Source: https://gist.github.com/jed/993585
-			var Xhr = window.XMLHttpRequest||ActiveXObject;
-			req = new Xhr('MSXML2.XMLHTTP.3.0');
+			var Xhr = context.XMLHttpRequest||ActiveXObject,
+					req = new Xhr('MSXML2.XMLHTTP.3.0');
 
 			req.open(type, url, true);
 			req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 			req.onreadystatechange = function (e) {
 				if (this.readyState === 4) {
 					if (this.status === 200) {
-						var response = prepareResponse(req.responseText);
-						fulfill(response);
+						var res = prepareResponse(req.responseText);
+						fulfill(res, req);
 					}
 					else {
 						reject(new Error('Request responded with status ' + req.status));
@@ -37,18 +59,48 @@
 		});
 	}
 
+/**
+	* Calls request() using the GET verb.
+	* 
+	* @param {string} url The url for the XHR request.
+	* 
+	* @memberof aias
+	*/
 	aias.get = function (url) {
 		return request('GET', url);
 	};
 
+/**
+	* Calls request() using the POST verb.
+	* 
+	* @param {string} url The url for the XHR request.
+	* @param {object} data Optional. The data to be passed with the request.
+	* 
+	* @memberof aias
+	*/
 	aias.post = function (url, data) {
 		return request('POST', url, data);
 	};
 
+/**
+	* Calls request() using the PUT verb.
+	* 
+	* @param {string} url The url for the XHR request.
+	* @param {object} data Optional. The data to be passed with the request.
+	* 
+	* @memberof aias
+	*/
 	aias.put = function (url, data) {
 		return request('PUT', url, data);
 	};
 
+/**
+	* Calls request() using the DELETE verb.
+	* 
+	* @param {string} url The url for the XHR request.
+	* 
+	* @memberof aias
+	*/
 	aias.delete = function (url) {
 		return request('DELETE', url);
 	};
@@ -65,6 +117,6 @@
 	}
 	// Global library
 	else {
-		this.aias = aias;
+		context.aias = aias;
 	}
-}).call(window);
+})(this);
